1f071f6a96cde670eea3f60d2d32f81bda72301b
BryanNsoh <bryan.anye.5@gmail.com>
2023-03-10 20:11:09 -0600

    Partial Changes 4 Linix 1

--- logger_query.py
+++ logger_query.py
@@ -9,36 +9,52 @@ import send_to_GCloud as GCloud
     See here for documentation: https://pycampbellcr1000.readthedocs.io/en/latest/index.html
 """
 
-datalogger = CR1000.from_url('serial:COM5:38400')
-#datalogger = CR1000.from_url('serial:/dev/ttyUSB0:38400')
+#datalogger = CR1000.from_url('serial:COM5:38400')
+datalogger = CR1000.from_url('serial:/dev/ttyUSB0:38400')
 
 # Getting names of tables containing data
 table_names = fxns.get_tables(datalogger)
+
+# Create file to store the last data withdrawal date
+date_file = "/date.txt"
+try:
+    time = device.gettime()
+    # _____ strip time to get only datetime
     
+    with open(date_file, 'w') as f:
+        stored_date = f.read()
+        exec(stored_date)   # Stores the previous date in a start variable
+except FileNotFoundError:
+    with open(date_file, 'x') as f:
+        start = device.gettime()
+        f.write("start = " + str(start)) # Stores the date at which to start
+        
+        
+# start at start time stored in file and stop now. 
+stop = device.gettime()
 
-start = datetime.datetime(2022, 8, 30 , 16, 0, 0)
-stop = datetime.datetime(2022, 8, 30 , 22, 0, 0)
-
-# Getting data to be stored
+# Pull data from table
 table_data = fxns.get_data(datalogger, table_names[1].decode('utf-8'), start, stop)
 
-local_file = r"C:\Users\Bryan\OneDrive - University of Nebraska-Lincoln\Documents\Research\Datalogger Cellular\Data Files\CR800data.json"
+local_backup = "/CR800data.json"
+to_Gcloud = ""
 
+
+# Save values to be appended to Bigquery in local file
 try:
     with open(local_file, 'w') as f:
         ndjson.dump(table_data,f)
 except FileNotFoundError:
     with open(local_file, 'x') as f:
         ndjson.dump(table_data,f)
-
-#send stored data to GCloud
+        
+# Send stored data to GCloud
 GCloud.send_to_cloud()
-    
-local_file = r"C:\Users\Bryan\OneDrive - University of Nebraska-Lincoln\Documents\Research\Datalogger Cellular\Data Files\CR800data.json"
 
+# Append data to local backup file
 try:
-    with open(local_file, 'w') as f:
+    with open(local_backup, 'w') as f:
         ndjson.dump(table_data,f)
 except FileNotFoundError:
-    with open(local_file, 'x') as f:
+    with open(local_backup, 'x') as f:
         ndjson.dump(table_data,f)
--- send_to_GCloud.py
+++ send_to_GCloud.py
@@ -3,14 +3,12 @@ from google.cloud import storage
 import os
 import ndjson
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Bryan\OneDrive - University of Nebraska-Lincoln\Documents\Research\Datalogger Cellular\apt-rite-378417-db62f3c0bfd6.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/apt-rite-378417-db62f3c0bfd6.json"
 
-local_file = r"C:\Users\Bryan\OneDrive - University of Nebraska-Lincoln\Documents\Research\Datalogger Cellular\Data Files\CR800data.json"
+local_file = "/CR800data.json"
 bucket_name = "logger1-bucket"
 blob_name = "plt-34/logger.json"
     
-
-
 def write_read(bucket_name, blob_name):
     """Write and read a blob from GCS using file-like IO"""
 
@@ -24,6 +22,7 @@ def write_read(bucket_name, blob_name):
     with open(local_file, "r") as from_, blob.open("w") as to_:
         to_.write(from_.read())
 
-def send_to_cloud(bucket_name, blob_name):
+def send_to_cloud():
     """Send data to cloud"""
     write_read(bucket_name, blob_name)
+    
