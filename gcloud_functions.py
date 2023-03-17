578114a3273a64b0570346e46c94b8db0b023d68
Bryan_Nsoh <bryan.anye.5@gmail.com>
2023-03-17 11:11:22 -0500

    Intermediate step

--- gcloud_functions.py
+++ gcloud_functions.py
@@ -1,18 +1,20 @@
 # Imports the Google Cloud client library
 from google.cloud import storage
 from google.cloud import bigquery
+from typing import List, Dict
 import os
 import json
-import pandas as pd
-from pandas_gbq import to_gbq
+import tempfile
 
 # Retreiving service account key using key_location.txt stored on local machine
-file_path = '~/keys/key_location.txt'
-expanded_file_path = os.path.expanduser(file_path)  # Replacing the ~ character with the expanded file name
+file_path = "~/keys/key_location.txt"
+expanded_file_path = os.path.expanduser(
+    file_path
+)  # Replacing the ~ character with the expanded file name
 print(expanded_file_path)
-with open(expanded_file_path, 'r') as f:
-        key = f.read()
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key
+with open(expanded_file_path, "r") as f:
+    key = f.read()
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key
 
 # Paths for google cloud
 local_file = "./CR800data.json"
@@ -24,7 +26,7 @@ project_id = "apt-rite-378417"
 dataset_id = "apt-rite-378417.loggertest1"
 table_id = "apt-rite-378417.loggertest1.SDIC"
 
-    
+
 def write_read(bucket_name, blob_name):
     """Write and read a blob from GCS using file-like IO"""
 
@@ -34,52 +36,90 @@ def write_read(bucket_name, blob_name):
 
     # Mode can be specified as wb/rb for bytes mode.
     # See: https://docs.python.org/3/library/io.html
-    
+
     with open(local_file, "r") as from_, blob.open("w") as to_:
         to_.write(from_.read())
 
+
 def update_bucket():
     """Send data to cloud"""
-    
+
     write_read(bucket_name, blob_name)
-    
-    
-def update_bqtable(table_data):
+
+
+def get_schema(list_dicts: List[Dict]) -> List[bigquery.SchemaField]:
+    """
+    Generate BigQuery schema for a list of dictionaries.
+
+    This function takes a list of dictionaries as input and generates a BigQuery schema
+    with appropriate field names and types.
+
+    Args:
+        list_dicts (List[Dict])-: A list of dictionaries with data.
+
+    Returns:
+        List[bigquery.SchemaField]: A list of BigQuery schema fields.
+    """
+
+    if not list_dicts:
+        return []
+
+    # Define a mapping of Python data types to BigQuery data types
+    type_mapping = {
+        int: "INTEGER",
+        float: "FLOAT",
+        str: "STRING",
+        bool: "BOOLEAN",
+        bytes: "BYTES",
+        # Add more mappings if needed
+    }
+
+    # Get the first dictionary from the list to infer schema
+    sample_dict = list_dicts[0]
+
+    # Generate BigQuery schema fields based on the sample dictionary
+    schema = [
+        bigquery.SchemaField(field_name, type_mapping[type(sample_value)])
+        for field_name, sample_value in sample_dict.items()
+    ]
+
+    return schema
+
+
+def update_bqtable(schema, table_data):
     """Update the bigquery table with newly obtained data from the logger"""
 
-    df = pd.DataFrame(table_data)
     bigquery_client = bigquery.Client()
 
-    
     job_config = bigquery.LoadJobConfig(
-        schema = [
-        bigquery.SchemaField("Datetime", "TIMESTAMP"),
-        bigquery.SchemaField("RecNbr", "INTEGER"),
-        bigquery.SchemaField("BatV", "FLOAT"),
-        bigquery.SchemaField("VWC_1", "FLOAT"),
-        bigquery.SchemaField("EC_1", "FLOAT"),
-        bigquery.SchemaField("ST_1", "FLOAT"),
-        bigquery.SchemaField("P_1", "INTEGER"),
-        bigquery.SchemaField("VWC_2", "FLOAT"),
-        bigquery.SchemaField("EC_2", "FLOAT"),
-        bigquery.SchemaField("ST_2", "FLOAT"),
-        bigquery.SchemaField("P_2", "INTEGER"),
-        bigquery.SchemaField("VWC_3", "FLOAT"),
-        bigquery.SchemaField("EC_3", "FLOAT"),
-        bigquery.SchemaField("ST_3", "FLOAT"),
-        bigquery.SchemaField("P_3", "INTEGER")
-        ],
+        schema=schema,
         autodetect=False,
-        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
     )
 
-    # Append the data to the existing BigQuery table
-    load_job = bigquery_client.load_table_from_json(table_data, table_id, job_config=job_config)
-    
-    # Wait for the job to complete
+    # Convert table_data (list of dicts) to newline-delimited JSON format
+    table_data_ndjson = "\n".join(json.dumps(item) for item in table_data)
+
+    # Write the data to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(table_data_ndjson.encode("utf-8"))
+        temp_file_path = temp_file.name
+
+    # Define the table reference using dataset_id and table_id
+    table_ref = bigquery_client.dataset(dataset_id).table(table_id)
+
+    # Load the data from the temporary file into BigQuery
+    with open(temp_file_path, "rb") as temp_file:
+        load_job = bigquery_client.load_table_from_file(
+            temp_file,
+            table_ref,
+            job_config=job_config,
+        )
+
+    # Wait for the load job to complete
     load_job.result()
 
-    # Print the number of rows inserted
-    print(f"{load_job.output_rows} rows inserted to {table_id}")
+    # Remove the temporary file
+    os.remove(temp_file_path)
 
-    
+    print(f"Loaded {load_job.output_rows} rows into {dataset_id}:{table_id}.")
--- logger_query_functions.py
+++ logger_query_functions.py
@@ -1,73 +1,107 @@
 from pycampbellcr1000 import CR1000
-import datetime
-import csv
+from datetime import timedelta
 import traceback
 import os
+import sys
+from datetime import datetime, timedelta
+import os.path
+import time
 
-    
+
 def get_tables(datalogger):
     """Gets a list of the names of the tables stored in the datalogger"""
     table_names = []
     while True:
-        try:
-            table_names = datalogger.list_tables()
-            #For testing
-            print("The table names are:")
-            print(table_names)
-        except Exception:
-            # Store exception in a text file in the local directory
-            try:
-                with open(os.path.join('', 'error_msg.txt'), "x") as err_msg:
-                    traceback.print_exc(limit=None,file=err_msg, chain=True)
-            except FileExistsError:
-                with open(os.path.join('', 'error_msg.txt'), "w") as err_msg:
-                    traceback.print_exc(limit=None,file=err_msg, chain=True)
-                
-        
+        table_names = datalogger.list_tables()
+        # For testing
+        print("The table names are:")
+        print(table_names)
+
         # If table_names is not empty, break (if statement true if not empty)
         # Otherwise, continue loop
-        if(table_names):
+        if table_names:
             return table_names
         else:
             print("Failed to get table names")
-            
-            
+
+
 def get_data(datalogger, table_name, start, stop):
     """Gets the data for a given table in the table_names list. leave blank to get all data"""
-    
+
     while True:
-        
+
         table_data = []
         cleaned_data = []
-        
+
         try:
             table_data = datalogger.get_data(table_name, start, stop)
-            # Cleaning table data 
+            # Cleaning table data
             for label in table_data:
-                dict_entry = {} 
+                dict_entry = {}
                 for key, value in label.items():
                     # Removing b' and ' characters from dict keys
-                    key = key.replace("b\'", "")
-                    key = key.replace("\'", "")
+                    key = key.replace("b'", "")
+                    key = key.replace("'", "")
                     dict_entry[key] = value
                     # Converting all datetime objects to ISO-formatted strings
-                    if isinstance(value, datetime.datetime):
+                    if isinstance(value, datetime):
                         dict_entry[key] = value.isoformat()
-                cleaned_data.append(dict_entry) 
-                
+                cleaned_data.append(dict_entry)
+
         except Exception:
             # Store exception in a text file in the local directory
             try:
-                with open(os.path.join('', 'error_msg.txt'), "x") as err_msg:
-                    traceback.print_exc(limit=None,file=err_msg, chain=True)
+                with open(os.path.join("", "error_msg.txt"), "x") as err_msg:
+                    traceback.print_exc(limit=None, file=err_msg, chain=True)
             except FileExistsError:
-                with open(os.path.join('', 'error_msg.txt'), "w") as err_msg:
-                    traceback.print_exc(limit=None,file=err_msg, chain=True)
-        
+                with open(os.path.join("", "error_msg.txt"), "w") as err_msg:
+                    traceback.print_exc(limit=None, file=err_msg, chain=True)
+
         # If something has been stored in table_data, exit loop.
         # Otherwise, continue
-        if(cleaned_data):
+        if cleaned_data:
             return cleaned_data
         else:
             print("failed to get data")
-            
+
+
+def parse_datetime_input(input_string):
+    try:
+        return datetime.fromisoformat(input_string)
+    except ValueError:
+        return None
+
+
+def track_and_manage_time(input_datetime=None, delay=60):
+    timekeeper_file = "./timekeeper.txt"
+
+    if input_datetime is not None:
+        input_datetime = parse_datetime_input(input_datetime)
+        if input_datetime is None:
+            print("Invalid datetime input.")
+            sys.exit(1)
+
+    if os.path.exists(timekeeper_file):
+        with open(timekeeper_file, "r") as f:
+            stored_datetime_str = f.read().strip()
+            stored_datetime = parse_datetime_input(stored_datetime_str)
+            if stored_datetime is None:
+                print("Invalid datetime in timekeeper file.")
+                sys.exit(1)
+            start = stored_datetime
+    else:
+        # Create the full file path by joining the directory and file name
+        full_path = os.path.join(os.getcwd(), timekeeper_file)
+        # Create timekeeper file
+        with open(full_path, "x") as f:
+            pass
+        start = input_datetime or datetime.now()
+
+    with open(timekeeper_file, "w") as f:
+        stop = start + timedelta(minutes=delay)
+        f.write(stop.isoformat())
+
+    # Delay for the given number of minutes
+    time.sleep(delay * 60)
+
+    return start, stop
