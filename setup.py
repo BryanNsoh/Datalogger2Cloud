8529ff204666fd46bc249b805d65f9d7d02b515e
Bryan_Nsoh <bryan.anye.5@gmail.com>
2023-03-26 11:25:42 -0500

    new setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import os
 import subprocess
-import shlex
+import shutil
+from pathlib import Path
 
 
-def run_command(command):
+def check_permissions():
+    if os.geteuid() != 0:
+        print(
+            "This script requires root privileges to manage systemd services and timers. Please run with sudo."
+        )
+        exit(1)
+
+
+def run_command(command, continue_on_error=False):
     print(f"Running Command: {''.join(command)}")
     result = subprocess.run(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True
@@ -11,7 +20,8 @@ def run_command(command):
 
     if result.returncode != 0:
         print(f"Error: {result.stderr}")
-        exit(1)
+        if not continue_on_error:
+            exit(1)
 
     return result.stdout, result.stderr
 
@@ -19,6 +29,19 @@ def run_command(command):
 def source_venv(venv_path, command):
     return f". {venv_path}/bin/activate && {command}"
 
+
+def create_file(file_name, content):
+    with open(file_name, "w") as f:
+        f.write(content)
+    return file_name
+
+
+def enable_and_start_systemd(unit_name):
+    run_command(f"sudo systemctl enable {unit_name}", continue_on_error=True)
+    run_command(f"sudo systemctl start {unit_name}", continue_on_error=True)
+
+
+check_permissions()
 
 # Set your project path and virtual environment path
 project_path = os.getcwd()
@@ -31,7 +54,7 @@ commands = [
 ]
 
 for cmd in commands:
-    stdout, stderr = run_command(cmd)
+    stdout, stderr = run_command(cmd, continue_on_error=True)
     print(stdout)
 
 # 2. Install and Upgrade Python Modules
@@ -43,41 +66,72 @@ commands = [
 ]
 
 for cmd in commands:
-    stdout, stderr = run_command(cmd)
+    stdout, stderr = run_command(cmd, continue_on_error=True)
     print(stdout)
 
-# 3.1 Create main_query.service
-service_content = f"""[Unit]
-Description=Run main_query.py after an Internet connection is established
-Wants=network-online.target main_query.timer
+shutdown_path = shutil.which("shutdown")
+rtcwake_path = shutil.which("rtcwake")
+sudo_path = shutil.which("sudo")
 
-[Service]
-Type=simple
-User=pi
-ExecStart={venv_path}/bin/python {project_path}/run_on_connection.sh
-Restart=on-failure
-"""
-
-with open("main_query.service", "w") as f:
-    f.write(service_content)
-
-# 3.2 Create main_query.timer
-timer_content = f"""[Unit]
-Description=Run main_query.service every hour
+# 3. Create and Configure Systemd Services and Timers
+units = [
+    # ...
+    {
+        "name": "shutdown",
+        "timer": f"""[Unit]
+Description=Run shutdown.service at 9 PM every day
 
 [Timer]
-OnBootSec=5min
-OnUnitActiveSec=1h
-Unit=main_query.service
+OnCalendar=*-*-* 21:00:00
+Unit=shutdown.service
 
 [Install]
 WantedBy=timers.target
-"""
+""",
+        "service": f"""[Unit]
+Description=Shut down the computer at 9 PM every day
 
-with open("main_query.timer", "w") as f:
-    f.write(timer_content)
+[Service]
+Type=oneshot
+ExecStart={sudo_path} {shutdown_path} -h now
+""",
+    },
+    {
+        "name": "rtcwake",
+        "timer": f"""[Unit]
+Description=Run rtcwake.service 1min before shutdown.service
 
-# 3.3 Create run_on_connection.sh
+[Timer]
+OnCalendar=*-*-* 20:59:00
+Unit=rtcwake.service
+
+[Install]
+WantedBy=timers.target
+""",
+        "service": f"""[Unit]
+Description=Power on the computer 8 hours after shutdown
+
+[Service]
+Type=oneshot
+ExecStart={sudo_path} {rtcwake_path} -m no -s 28800
+""",
+    },
+]
+
+# Create, enable, and start the systemd units
+for unit in units:
+    service_file = create_file(f"{unit['name']}.service", unit["service"])
+    timer_file = create_file(f"{unit['name']}.timer", unit["timer"])
+    run_command(
+        f"sudo cp {service_file} /etc/systemd/system/{service_file}",
+        continue_on_error=True,
+    )
+    run_command(
+        f"sudo cp {timer_file} /etc/systemd/system/{timer_file}", continue_on_error=True
+    )
+    enable_and_start_systemd(service_file)
+
+
 run_on_connection_content = f"""#!/bin/bash
 # Wait for an internet connection
 while ! ping -c 1 -W 1 8.8.8.8; do
@@ -91,19 +145,7 @@ sudo chronyc -a makestep
 {venv_path}/bin/python {project_path}/main_query.py
 """
 
-with open("run_on_connection.sh", "w") as f:
-    f.write(run_on_connection_content)
+create_file("run_on_connection.sh", run_on_connection_content)
 
 # Make the script executable
-run_command("chmod +x run_on_connection.sh")
-
-# Copy the systemd service and timer files
-run_command("sudo cp main_query.service /etc/systemd/system/main_query.service")
-run_command("sudo cp main_query.timer /etc/systemd/system/main_query.timer")
-
-# Enable the systemd service and timer
-run_command("sudo systemctl enable main_query.service")
-run_command("sudo systemctl enable main_query.timer")
-
-# Start the systemd service and timer
-run_command("sudo systemctl start main_query.service")
+run_command("chmod +x run_on_connection.sh", continue_on_error=True)
